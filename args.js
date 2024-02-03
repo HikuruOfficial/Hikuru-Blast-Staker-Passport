aeb72b3901a02487905c43eb394a9f39be3e0a88
Hikuru Official <132744928+HikuruOfficial@users.noreply.github.com>
2024-02-03 12:24:12 +0100

    Create args.js

---
+++ args.js
@@ -0,0 +1,5 @@
+module.exports = [
+    "0x2D1CC54da76EE2aF14b289527CD026B417764fAB",
+    "0x1bCec961363dC355558421E8a66423006aB75a25",
+    "https://hikuru.com/statics/nft/blast-passport/0.json",
+  ];
